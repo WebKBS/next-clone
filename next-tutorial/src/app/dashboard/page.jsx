144bc2e8f441a1a7b8681643541431058b49f4bc
gangbeomsu <kbs21c1987@gmail.com>
2023-08-20 15:08:07 +0900

    useEffect대신 swr사용

--- next-tutorial/src/app/dashboard/page.jsx
+++ next-tutorial/src/app/dashboard/page.jsx
@@ -1,5 +1,46 @@
-function Users() {
-  return <div>Users</div>;
+"use client";
+
+// import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+function Dashboard() {
+  // useEffect는 더이상 쓰지말자.
+  // const [data, setData] = useState([]);
+  // const [err, setErr] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   async function getData() {
+  //     setIsLoading(true);
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts/", {
+  //       // next: { revalidate: 10 },
+  //       cache: "no-store",
+  //     });
+
+  //     if (!res.ok) {
+  //       setErr(true);
+  //     }
+
+  //     const data = await res.json();
+
+  //     setData(data);
+  //     setIsLoading(false);
+  //   }
+
+  //   getData();
+  // }, []);
+
+  // console.log(data);
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts/",
+    fetcher
+  );
+  console.log(data);
+
+  return <div>Dashboard</div>;
 }
 
-export default Users;
+export default Dashboard;
