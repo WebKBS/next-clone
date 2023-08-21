bfcb64ad6b7dcf985069441a1eff51303e4e75f9
gangbeomsu <kbs21c1987@gmail.com>
2023-08-21 21:04:26 +0900

    get 수정 완료

--- next-tutorial/src/app/api/posts/route.js
+++ next-tutorial/src/app/api/posts/route.js
@@ -1,38 +1,83 @@
-import connect from "@/utils/db";
+import mysql from "mysql2/promise";
 import { NextResponse } from "next/server";
-import Post from "@/models/Post";
 
+const createConnection = async () => {
+  const connection = await mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  });
+
+  try {
+    await connection.connect();
+    console.log("MySQL 데이터베이스에 연결되었습니다.");
+    return connection;
+  } catch (error) {
+    console.error("MySQL 연결 실패:", error);
+    throw new Error("데이터베이스 연결 실패");
+  }
+};
 export const GET = async (request) => {
-  const url = new URL(request.url);
-
-  const username = url.searchParams.get("username");
-
   try {
-    await connect();
+    const connection = await createConnection();
 
-    const posts = await Post.find(username && { username });
+    const url = new URL(request.url);
+    const username = url.searchParams.get("username");
 
-    return new NextResponse(JSON.stringify(posts), { status: 201 });
-  } catch (err) {
-    console.log(err);
+    let query = "SELECT * FROM posts";
+    let queryParams = [];
+
+    if (username) {
+      query += " WHERE username = ?";
+      queryParams.push(username);
+    }
+
+    const [rows, fields] = await connection.query(query, queryParams);
+
+    // 데이터베이스 연결 해제
+    connection.end();
+
+    return new NextResponse(JSON.stringify(rows), { status: 200 });
+  } catch (error) {
+    console.log(error);
     return new NextResponse("데이터베이스 에러", { status: 500 });
   }
 };
 
-export const POST = async (request) => {
-  const body = await request.json();
+// 몽고 디비
 
-  const newPost = new Post(body);
-  console.log(newPost);
+// export const GET = async (request) => {
+//   const url = new URL(request.url);
 
-  try {
-    await connect();
+//   const username = url.searchParams.get("username");
 
-    await newPost.save();
+//   try {
+//     await connect();
 
-    return new NextResponse("Post 성공!", { status: 201 });
-  } catch (err) {
-    console.log(err);
-    return new NextResponse("데이터베이스 에러", { status: 500 });
-  }
-};
+//     const posts = await Post.find(username && { username });
+
+//     return new NextResponse(JSON.stringify(posts), { status: 201 });
+//   } catch (err) {
+//     console.log(err);
+//     return new NextResponse("데이터베이스 에러", { status: 500 });
+//   }
+// };
+
+// export const POST = async (request) => {
+//   const body = await request.json();
+
+//   const newPost = new Post(body);
+//   console.log(newPost);
+
+//   try {
+//     await connect();
+
+//     await newPost.save();
+
+//     return new NextResponse("Post 성공!", { status: 201 });
+//   } catch (err) {
+//     console.log(err);
+//     return new NextResponse("데이터베이스 에러", { status: 500 });
+//   }
+// };
--- next-tutorial/src/app/blog/page.jsx
+++ next-tutorial/src/app/blog/page.jsx
@@ -38,7 +38,7 @@ const Blog = async () => {
           </div>
           <div className={styles.content}>
             <h1 className={styles.title}>{item.title}</h1>
-            <p className={styles.desc}>{item.desc}</p>
+            <p className={styles.desc}>{item.description}</p>
           </div>
         </Link>
       ))}
